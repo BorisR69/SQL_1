78cac0333d15b701576c73eb534f1aa17545ea01
Boris Rozov <outsource@list.ru>
2023-06-08 21:20:19 +0300

    Initial commit

---
+++ src/main/java/org/example/fill_table.sql
@@ -0,0 +1,5 @@
+insert into persons values ('Иван', 'Петров', 28, '+79283334455','Moscow');
+insert into persons values ('Семен', 'Хохлов', 21, '+79243334453','Krasnodar');
+insert into persons values ('Дмитрий', 'Чендек', 35, '+79283334455','Kazan');
+insert into persons values ('Владимир', 'Тополев', 44, '+79283334455','Moscow');
+insert into persons values ('Константин', 'Петров', 18, '+79283334455','Omsk')
